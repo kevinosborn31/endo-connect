c8a64a2b8545fb050463a94b784f43819bbdb5af
Kevin Osborn <kevinosborn31@gmail.com>
2020-02-24 17:49:14 +1100

    started seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_113530) do
+ActiveRecord::Schema.define(version: 2020_02_24_063407) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+  # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Answer.destroy_all
+Question.destroy_all
+Blood_sugar.destroy_all
+Glycemic_index.destroy_all
+Medication.destroy_all
+User.destroy_all
+
+a1 = Job.create!(company_name: '1-Stop Connections' , location: 'Sydney', description: '', salary: 80000, role: "Junior Front End Developer",  applicants: 0, user_id: u3.id )
